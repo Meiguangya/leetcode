aae6b665af4d8d6892127c59317917d3535835c3
梅光亚 <guangya.mei@extremevision.com.cn>
2025-08-02 16:20:23 +0800

    使用hash完成查找最长连续子序列  时间复杂度O(2n) -> O(n)
    关键在于每次看比他小的在不在set里面 如果在的话，那就跳过，不在就从这次最小的开始，保证每次从一个序列的最前面开始找子序列

---
+++ src/main/java/SwordFingerOffer/hash/LongestConsecutive.java
@@ -0,0 +1,55 @@
+package main.java.SwordFingerOffer.hash;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author meiguangya
+ * @date 2025/8/2 下午4:09
+ * @description 最长连续子序列
+ * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+ *
+ * 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [100,4,200,1,3,2]
+ * 输出：4
+ * 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+ */
+public class LongestConsecutive {
+
+
+    public int longestConsecutive(int[] nums) {
+
+        Set<Integer> numSet = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            numSet.add(nums[i]);
+        }
+
+        int ret = 1;
+
+        for (Integer num : numSet) {
+
+            if (numSet.contains(num - 1)) {
+                continue;
+            }
+
+            int len = 1;
+
+            while(numSet.contains(num + 1)){
+                len = len + 1;
+                num = num + 1;
+            }
+
+            ret = Math.max(len,ret);
+
+        }
+
+        return ret;
+
+    }
+
+
+}
